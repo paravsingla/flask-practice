371f68d1688441525e662677a11f2c11d03baaa5
parasing <parasing@qti.qualcomm.com>
2019-11-21 10:15:13 +0530

    Add migrations

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
 from flask import Flask
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import *
 
 app  = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
-
-class User(db.Model):
-	id = db.Column(db.Integer(), primary_key = True)
-	username = db.Column(db.String(255))
-	password = db.Column(db.String(255))
-	posts = db.relationship('Post', backref='user', lazy='dynamic')
-
-	def __init__(self, username):
-		self.username = username
-
-	def __repr__(self):
-		return "<User '{}'>".format(self.username)
+migrate = Migrate(app, db)
 
 
-class Post(db.Model):
-	id = db.Column(db.Integer(), primary_key=True)
-	title = db.Column(db.String(255))
-	text = db.Column(db.Text())
-	publish_date = db.Column(db.DateTime())
-	user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
-	comments = db.relationship('Comment', backref='post', lazy='dynamic')
-
-	def __init__(self, title):
-		self.title = title
-
-	def __repr__(self):
-		return "<Post '{}'>".format(self.title)
-
-class Comment(db.Model):
-	id = db.Column(db.Integer(), primary_key=True)
-	name = db.Column(db.String(255))
-	text = db.Column(db.Text())
-	date = db.Column(db.DateTime())
-	post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
-
-	def __repr__(self):
-		return "<Comment '{}'>".format(self.text[:15])
 
 @app.route('/')
 def home():
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from main import app, db, User, Post
+from main import app, db, migrate
+from models import User, Post, Tag, Comment
 
 @app.shell_context_processor
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Post=Post)
+	return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, Comment=Comment, migrate=migrate)
